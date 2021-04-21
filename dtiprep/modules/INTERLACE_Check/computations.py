ADD: Baseline average module implemented. a few minor bug fixed

dtiprep/modules/INTERLACE_Check/computations.py
@@ -187,7 +187,8 @@ def rigid_3d(static,moving,
              nbins=32,
              level_iters=[10000,1000,100],
              sigmas=[3.0,1.0,0.0],
-             factors=[4,2,1]):
+             factors=[4,2,1],
+             sampling_prop=None):
     ## Make affine map
     identity=np.eye(4)
     affine_map=AffineMap(identity,static.shape, affine_static,
@@ -201,12 +202,7 @@ def rigid_3d(static,moving,
     transformed=c_of_mass.transform(moving)
 
     ## registration preparation
-    nbins=32
-    sampling_prop = None
     metric= MutualInformationMetric(nbins,sampling_prop)
-    level_iters=[10000,1000,100]
-    sigmas = [3.0, 1.0, 0.0]
-    factors= [4,2,1]
     affreg= AffineRegistration(metric=metric, 
                                level_iters=level_iters, 
                                sigmas=sigmas, 
@@ -241,7 +237,8 @@ def rigid_2d(static,moving,
              nbins=32,
              level_iters=[10000,1000,100],
              sigmas=[3.0,1.0,0.0],
-             factors=[4,2,1]):
+             factors=[4,2,1],
+             sampling_prop=None):
     ## Make affine map
     identity=np.eye(3)
     affine_map=AffineMap(identity,static.shape, affine_static,
@@ -255,7 +252,6 @@ def rigid_2d(static,moving,
     transformed=c_of_mass.transform(moving)
 
     ## registration preparation
-    sampling_prop = None
     metric= MutualInformationMetric(nbins,sampling_prop)
     affreg= AffineRegistration(metric=metric, 
                                level_iters=level_iters, 
c2de185da6ba95ccdcb67793cd7babd59c002d99
skpark <scalphunter@gmail.com>
2022-04-11 15:32:10 -0400

    DOC: version tag updated

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.2.6-beta4"
+    "version" : "0.2.6-beta5"
   },
   "dmriprep": {
-    "version" : "0.2.6-beta4"
+    "version" : "0.2.6-beta5"
   },
   "dtiatlasbuilder": {
     "version" : "0.1.0"

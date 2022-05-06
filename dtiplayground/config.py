7253dc737301d9f1766bebd8e9af43f4a9bf723e
skpark <scalphunter@gmail.com>
2022-05-06 16:29:04 -0400

    DOC: version 0.3.0-beta5

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.3.0-beta4"
+    "version" : "0.3.0-beta5"
   },
   "dmriprep": {
-    "version" : "0.3.0-beta4"
+    "version" : "0.3.0-beta5"
   },
   "dtiatlasbuilder": {
     "version" : "0.1.0"

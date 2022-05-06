afedb767fd4bb33e47a4466f7c84c265a31f5037
skpark <scalphunter@gmail.com>
2022-05-06 13:53:17 -0400

    CHG: dmriprep executable has file mapping glitch, resolved

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.3.0-beta1"
+    "version" : "0.3.0-beta4"
   },
   "dmriprep": {
-    "version" : "0.3.0-beta1"
+    "version" : "0.3.0-beta4"
   },
   "dtiatlasbuilder": {
     "version" : "0.1.0"

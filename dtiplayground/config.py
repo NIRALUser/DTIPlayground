1ea736c28b88bf93f397639b1234f0dadf592c31
SK Park <scalphunter@gmail.com>
2022-06-03 16:33:46 -0400

    DOC: version 0.3.4b1

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.3.3b1"
+    "version" : "0.3.4b1"
   },
   "dmriprep": {
-    "version" : "0.3.3b1"
+    "version" : "0.3.4b1"
   },
   "dtiatlasbuilder": {
     "version" : "0.1.0"

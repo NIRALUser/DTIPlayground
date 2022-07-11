e019410076458125a67269ceedac2528e8495fd0
SK Park <scalphunter@gmail.com>
2022-07-11 14:40:19 -0400

    BIG: minor bug fixed.

--- dtiplayground/dmri/preprocessing/modules/SINGLETRACT_Process/tractography.py
+++ dtiplayground/dmri/preprocessing/modules/SINGLETRACT_Process/tractography.py
@@ -57,46 +57,20 @@ def compute(tensorImage, labelmapImage, output_dir, scalar='fa' , axis=0):
     
 
     img = tensorImage.images
-    # logger("{} was chosen for the scalars".format(scalar_function_map[scalar]['message']),prep.Color.INFO)
-    # logger("Computing {}...".format(scalar_function_map[scalar]['message']),prep.Color.PROCESS)
-    # img_scalar = np.apply_along_axis(scalar_function_map[scalar]['func'], axis, img)
+    logger("{} was chosen for the scalars".format(scalar_function_map[scalar]['message']),prep.Color.INFO)
+    logger("Computing {}...".format(scalar_function_map[scalar]['message']),prep.Color.PROCESS)
+    img_scalar = np.apply_along_axis(scalar_function_map[scalar]['func'], axis, img)
     img_label = labelmapImage.images
 
     scalar_file = Path(output_dir).joinpath('{}.nrrd'.format(scalar)).__str__()
 
-    # newImg = deepcopy(tensorImage)
-    # newImg.updateImage3D(img_scalar)
-    # newImg.writeImage(scalar_file, dest_type='nrrd', dtype='float')
+    newImg = deepcopy(tensorImage)
+    newImg.updateImage3D(img_scalar)
+    newImg.writeImage(scalar_file, dest_type='nrrd', dtype='float')
 
     affine=tensorImage.getAffineMatrixForNifti()
-    
-    # logger("Computing Geodesic Anisotropy...",prep.Color.PROCESS)
-    # img_ga = np.apply_along_axis(ga, 0, img)
-    # print(img_ga.shape)
+
     labels=img_label
-    seed_mask = labels == 2
-    white_matter = (labels == 1) | (labels == 2)
-    seeds = utils.seeds_from_mask(seed_mask, affine, density=1)
-    response, ratio = auto_response_ssst(gtab, data, roi_radii=10, fa_thr=0.7)
-    csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order=6)
-    csd_fit = csd_model.fit(data, mask=white_matter)
-
-    csa_model = CsaOdfModel(gtab, sh_order=6)
-    gfa = csa_model.fit(data, mask=white_matter).gfa
-
-    stopping_value = 0.25
-    stopping_criteria = ThresholdStoppingCriterion(img_scalar, stopping_value)
-
-
-    stopping_criterion = ThresholdStoppingCriterion(gfa, .25)
-    detmax_dg = DeterministicMaximumDirectionGetter.from_shcoeff(
-    csd_fit.shm_coeff, max_angle=30., sphere=default_sphere)
-    streamline_generator = LocalTracking(detmax_dg, stopping_criterion, seeds,
-                                         affine, step_size=.5)
-    streamlines = Streamlines(streamline_generator)
-
-    sft = StatefulTractogram(streamlines, hardi_img, Space.RASMM)
-    save_trk(sft, "tractogram_deterministic_dg.trk")
 
 
     exit(0)

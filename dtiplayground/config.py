5138073e8a2911a552e67595b68c81ab7e1044c5
jdubos <johannadubos32@gmail.com>
2022-08-11 16:33:33 -0400

    STYLE: update version

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.3.8b4"
+    "version" : "0.3.8b5"
   },
   "dmriprep": {
-    "version" : "0.3.8b4"
+    "version" : "0.3.8b5"
 
   },
   "dtiatlasbuilder": {

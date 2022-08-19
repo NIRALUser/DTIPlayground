ac0966952068ec2b91b97fc442ee4b16c6ac88cb
SK Park <scalphunter@gmail.com>
2022-08-19 01:19:26 -0400

    BUG: pandas missing in requirements

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.4.1b1"
+    "version" : "0.4.1"
   },
   "dmriprep": {
-    "version" : "0.4.1b1"
+    "version" : "0.4.1"
   },
   "dmriatlas": {
     "version" : "0.1.0"

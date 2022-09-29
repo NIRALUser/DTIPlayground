d90a35d6903f42867b9cc5ae845a325025204e9c
SK Park <scalphunter@gmail.com>
2022-09-29 15:02:55 -0400

    .

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,10 +1,10 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.4.4"
+    "version" : "0.4.5b1"
   },
   "dmriprep": {
-    "version" : "0.4.4"
+    "version" : "0.4.5b1"
   },
   "dmriatlas": {
     "version" : "0.1.0"

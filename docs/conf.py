69b96417cbeaca47810ba9c32f62132deb155ded
SK Park <scalphunter@gmail.com>
2022-10-07 08:40:00 -0400

    DOC: sphinx doc init

--- docs/conf.py
+++ docs/conf.py
@@ -5,23 +5,47 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+from datetime import datetime
+from sphinx import __version__ as sphinxversion
+import sphinx_rtd_theme
+from pathlib import Path
+
+sys.path.append(Path(__file__).parent.joinpath('ext').__str__())
+from github_link import make_linkcode_resolve
 
 project = 'DTIPlayground'
 copyright = '2022, Neuro Image Research and Analysis Laboratory, University of North Carolina @ Chapel Hill'
-author = 'SK Park'
+author = 'Neuro Image Research and Analysis Laboratory'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.linkcode',
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+linkcode_resolve = make_linkcode_resolve('dtiplayground',
+                                         u'https://github.com/niralUser/'
+                                         'dtiplayground/blob/{revision}/'
+                                         '{package}/{path}#L{lineno}')
+def setup(app):
+    pass
+    app.add_css_file('overrides.css')
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to DTIPlayground's documentation!
-=========================================
+DTIPlayground - The integrated DWI processing software
+======================================================
 
 .. toctree::
    :maxdepth: 2

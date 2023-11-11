4cafd53d6459f85a33ac4b21b8380466777dc99d
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-11-10 20:07:15 -0500

    basic skeleton calling fiberprocess with correct arguments.

--- dtiplayground/dmri/common/tools/fiberprocess.py
+++ dtiplayground/dmri/common/tools/fiberprocess.py
@@ -9,13 +9,44 @@ class FiberProcess(ExternalToolWrapper):
         elif 'softwares' in kwargs:
             self.binary_path=kwargs['softwares']['fiberprocess']['path']
 
-    def run(self, datasheet_path, atlas_path, tracts, properties_to_profile, output_path, options=[]):
+    """
+    FiberProcess Command Line Tool
+
+    Usage:
+      fiberprocess [options]
+
+    Options:
+      # I/O
+      --fiber_file, -fiberFile           Input DTI fiber file. (Required)
+      --fiber_output, -o                 Output fiber file. May be warped or updated with new data. (Required)
+      --tensor_volume, -T                Input tensor volume. Interpolate tensor values from this field. (Required)
+      --scalarImage, -S                  Input scalar image file. Samples scalar values at fiber locations.
+      --scalarName                        Name for the pointData field in the output fiber file.
+      --h_field, -H                      Input HField for warp and statistics lookup.
+      --displacement_field, -D           Input Displacement Field for warp and statistics lookup.
+
+      # Options
+      --saveProperties, -p               Save tensor property as scalar data into the VTK (for VTK fiber files).
+      --no_warp, -n                      Do not warp the geometry of tensors, only obtain new statistics.
+      --fiber_radius, -R                 Set the radius of all fibers to this value.
+      --index_space, -indexSpace         Use index-space for fiber output coordinates; otherwise, use world space.
+
+      # Voxelize
+      --voxelize, -V                     Voxelize fiber into a label map. Deformation is applied before voxelization.
+      --voxelize_count_fibers           Count the number of fibers per voxel instead of just setting to 1.
+      --voxel_label                       Label for voxelized fiber (default is 1).
+
+      # Advanced Options
+      --verbose, -v                      Produce verbose output.
+      --noDataChange                     Do not change data.
+    """
+    """
+    Calls the FiberProcess binary using the Python wrapper in the common tools. Uses arguments described above.
+    """
+    def run(self, fiber_file, output_path, options=[]):
         arguments=[]
-        arguments+=['--datasheet',datasheet_path]
-        arguments+=['--atlas',atlas_path]
-        # arguments+=['--tracts',tracts]
-        # arguments+=['--propertiesToProfile',properties_to_profile]
-        # arguments+=['--output',output_path]
+        arguments+=['--fiber_file',fiber_file]
+        arguments+=['--fiber_output', output_path]
         arguments+=options
         self.setArguments(arguments)
         return self.execute(arguments)
--- dtiplayground/dmri/fiberprofile/modules/EXTRACT_Profile/EXTRACT_Profile.py
+++ dtiplayground/dmri/fiberprofile/modules/EXTRACT_Profile/EXTRACT_Profile.py
@@ -1,38 +1,84 @@
-import dtiplayground.dmri.fiberprofile as base
-import dtiplayground.dmri.common as common
-import csv
-import yaml
-
 from pathlib import Path
 
+import pandas as pd
+
+import dtiplayground.dmri.common as common
+import dtiplayground.dmri.fiberprofile as base
 from dtiplayground.dmri.common import tools
 
-logger=common.logger.write
+logger = common.logger.write
+
 
 class EXTRACT_Profile(base.modules.DTIFiberProfileModule):
-    def __init__(self,config_dir,*args,**kwargs):
+    def __init__(self, config_dir, *args, **kwargs):
         super().__init__(config_dir)
-        
-    def generateDefaultProtocol(self,image_obj):
+
+    def generateDefaultProtocol(self, image_obj):
         super().generateDefaultProtocol(image_obj)
         ## todos
         return self.protocol
+
     """
     Main function called by the pipeline when this module runs.
     """
-    def process(self,*args,**kwargs):
+
+    def process(self, *args, **kwargs):
         super().process()
-        inputParams=self.getPreviousResult()['output']
+        inputParams = self.getPreviousResult()['output']
         protocol_options = args[0]
         self.software_info = protocol_options['software_info']['softwares']
         # << TODOS>>
         path_to_csv = inputParams["file_path"]
-        with open(path_to_csv) as csv_file:
-            reader = csv.reader(csv_file)
-            header = next(csv_file)
-            print(header)
-            for row in reader:
-                print(row)
+        df = pd.read_csv(path_to_csv)
+        atlas_path = self.protocol["atlas"]
+        tracts = self.protocol["tracts"].split(',')
+        output_base_dir = self.output_dir  # output directory string
+        properties_to_profile = self.protocol["propertiesToProfile"].split(',')
+
+        row = df.iloc[1]
+        path_to_dti_image = row.iloc[1]
+        # for index, row in df.itterows():
+        #     path_to_dti_image = row.iloc[0]
+
+        # For each subject
+
+        # For each property to profile,
+
+        for property in properties_to_profile:
+            print("property: ", property)
+            # create the directory for the output for the scalar property
+            scalar_dir_output_path = Path(output_base_dir).joinpath(property)
+            scalar_dir_output_path.mkdir(parents=True, exist_ok=True)
+            scalar_img_path = row.iloc[2]
+            # create the file path for the output
+            for tract in tracts:
+                print("tract: ", tract)
+                tract_absolute_filename = atlas_path + tract # concatenate the atlas path with the tract name
+                fiber_output_path = scalar_dir_output_path.__str__() + '/' + tract.replace('_extracted_done',
+                                                                                              f'_{property}_profile')
+                fiber_output_path_str = fiber_output_path
+                scalar_name = property
+                options = []
+                options += ['--scalarName', scalar_name]
+                options += ['--ScalarImage', scalar_img_path]
+                options += ['--no_warp']
+                fiberprocess = tools.FiberProcess(self.software_info['fiberprocess']['path'])
+                fiberprocess.run(tract_absolute_filename, fiber_output_path_str,
+                                 options=options)
+
+            # Extract the filename without the path
+
+            # call fiberprocess
+            self.runFiberProcess()
+            # call fiberpostprocess
+            self.runFiberPostProcess()
+            # call dtitractstat
+            self.runDTITractStat()
+
+            # create the file path for the output
+
+        for index, row in df.itterows():
+            path_to_dti_image = row.iloc[0]
 
         # run fiberprocess
         self.runFiberProcess()
@@ -41,11 +87,13 @@ class EXTRACT_Profile(base.modules.DTIFiberProfileModule):
         # call dtitractstat
         # self.runDTITractStat()
 
-        self.result['output']['success']=True
+        self.result['output']['success'] = True
         return self.result
+
     """
     Runs the fiberprocess binary using the Python wrapper in the common tools.
     """
+
     def runFiberProcess(self, args=None):
         inputParams = self.getPreviousResult()['output']
         path_to_csv = inputParams["file_path"]
@@ -56,16 +104,11 @@ class EXTRACT_Profile(base.modules.DTIFiberProfileModule):
 
         # call fiberprocess
         print(self.software_info)
-        fiberprocess = tools.FiberProcess(self.software_info['fiberprocess']['path'])
-        # TODO: what type of file is the output of fiberprocess?
-        fiberprocess_output_path = Path(self.output_dir).joinpath('fiberprocess_out.csv').__str__()
-        options = []
-        fiberprocess.run(path_to_csv, atlas_path, tracts, properties_to_profile, fiberprocess_output_path,
-                         options=options)
 
     """
     Runs the fiberpostprocess binary using the Python wrapper in the common tools.
     """
+
     def runFiberPostProcess(self, args=None):
         inputParams = self.getPreviousResult()['output']
         path_to_csv = inputParams["file_path"]
@@ -79,10 +122,12 @@ class EXTRACT_Profile(base.modules.DTIFiberProfileModule):
         fiberpostprocess_output_path = Path(self.output_dir).joinpath('tensor.nrrd').__str__()
         options = []
         fiberpostprocess.run(path_to_csv, atlas_path, tracts, properties_to_profile, fiberpostprocess_output_path,
-                         options=options)
+                             options=options)
+
     """
     Runs the dtitractstat binary using the Python wrapper in the common tools.
     """
+
     def runDTITractStat(self, args=None):
         inputParams = self.getPreviousResult()['output']
         path_to_csv = inputParams["file_path"]

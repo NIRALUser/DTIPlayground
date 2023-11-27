b9e00851b525cd1998ecd87193ebeae1e885504c
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-11-27 16:25:47 -0500

    added print statement

--- dtiplayground/dmri/common/tools/dtiprocess.py
+++ dtiplayground/dmri/common/tools/dtiprocess.py
@@ -29,4 +29,5 @@ class DTIProcess(ExternalToolWrapper):
             arguments += [scalar_opt, outputFileStem + '_' + scalar + '.nrrd']
         arguments += options
         self.setArguments(arguments)
+        print(self.binary_path)
         return self.execute(arguments)

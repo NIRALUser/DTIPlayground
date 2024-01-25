5e850458a9276b8d542033b2b687f627f769db91
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2024-01-25 15:36:51 -0500

    added fvp_data read step back

--- dtiplayground/dmri/fiberprofile/modules/EXTRACT_Profile/EXTRACT_Profile.py
+++ dtiplayground/dmri/fiberprofile/modules/EXTRACT_Profile/EXTRACT_Profile.py
@@ -143,6 +143,10 @@ class EXTRACT_Profile(base.modules.DTIFiberProfileModule):
                         dtitractstat = tools.DTITractStat(self.software_info['dtitractstat']['path'])
                         dtitractstat.run(fiberpostprocess_output_path, dtitractstat_output_path, options=options)
 
+                    # extract fvp data
+                    fvp_data = pd.read_csv(dtitractstat_output_path, skiprows=[0, 1, 2, 3])
+                    logger(fvp_data.head().__str__())
+                    logger(fvp_data.tail().__str__())
                     # write fvp data to csv
                     if tract_stat_df is None:
                         col_list = ['case_id'] + fvp_data["Arc_Length"].tolist()

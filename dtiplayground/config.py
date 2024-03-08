3ba0014cb95624dc7e06c8b429a7008b3623d15c
Martin Styner <styner@unc.edu>
2024-03-08 17:33:07 -0500

    BUG: dipy versioning

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,13 +1,13 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.5.8b4"
+    "version" : "0.5.8b5"
   },
   "dmriplayground": {
-    "version" : "0.5.8b4"
+    "version" : "0.5.8b5"
   },
   "dmriprep": {
-    "version" : "0.5.8b4"
+    "version" : "0.5.8b5"
   },
   "dmriatlas": {
     "version" : "0.2.0b1"

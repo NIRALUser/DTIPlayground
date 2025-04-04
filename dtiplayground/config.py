2d33a6cc6dfd11b6cb19dcd56dd78d5f923bd9c8
Martin Styner <styner@unc.edu>
2025-04-04 13:46:49 -0400

    release 0.5.9b4

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,13 +1,13 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.5.9b3"
+    "version" : "0.5.9b4"
   },
   "dmriplayground": {
-    "version" : "0.5.9b3"
+    "version" : "0.5.9b4"
   },
   "dmriprep": {
-    "version" : "0.5.9b3"
+    "version" : "0.5.9b4"
   },
   "dmriatlas": {
     "version" : "0.2.0b1"

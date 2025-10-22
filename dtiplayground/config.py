cbbf297807ce5366451b4941c0a92c157e02cad7
Martin Styner <styner@unc.edu>
2025-10-22 09:25:43 +0200

    Update config.py

--- dtiplayground/config.py
+++ dtiplayground/config.py
@@ -1,13 +1,13 @@
 
 INFO = {
   "dtiplayground": { 
-    "version" : "0.5.11.1"
+    "version" : "0.5.11.2"
   },
   "dmriplayground": {
-    "version" : "0.5.11.1"
+    "version" : "0.5.11.2"
   },
   "dmriprep": {
-    "version" : "0.5.11.1"
+    "version" : "0.5.11.2"
   },
   "dmriatlas": {
     "version" : "0.2.0b1"
